8ca4c9e9b1337c34f81179a15b4e2ca741487620
Sxarmat <sxarmat@yandex.ru>
2022-07-04 22:25:47 +0500

    Added a description of the function and the search for the shortest route to all addresses from the condition.

--- route.py
+++ route.py
@@ -1,18 +1,21 @@
 from itertools import permutations
 from math import sqrt, pow
 
-post_in = (0, 2)
-addresses_in = ((2, 5), (5, 2), (6, 6), (8, 3))
 
-
-def find_route(post, *addresses):
+def find_route(post, addresses):
+    '''
+    Ищет все возможные варианты маршрута, сравнивает их длину и возвращает наиболее короткий из них
+    :param post: координаты почтового отделения
+    :param addresses: список, кортеж или множество, содержащие координаты всех точек маршрута
+    :return: строка с последовательностью всех точек, которые составляют самый короткий из маршрутов с выводом
+    промежуточных расстояний для каждой точки (от начала до текущей точки) и общей длины маршрута.
+    '''
     shortest_route = ''
     shortest_distance = float('inf')
     start = stop = post
     waypoints = permutations(addresses, len(addresses))
     for waypoint in waypoints:
         route = calc_distance((start, *waypoint, stop))
-        print(route)
         if route[1] < shortest_distance:
             shortest_route = route[0]
             shortest_distance = route[1]
@@ -20,6 +23,12 @@ def find_route(post, *addresses):
 
 
 def calc_distance(route):
+    '''
+    Формирует строку с последовательностью всех точек маршрута, с выводом промежуточных расстояний для каждой точки и
+    считает длину маршрута
+    :param route: последовательность координат маршрута
+    :return: кортеж с полным маршрутом и его длиной
+    '''
     route_str = '{}'.format(route[0])
     distance = 0
     for point in range(len(route) - 1):
@@ -30,6 +39,7 @@ def calc_distance(route):
     return route_str, distance
 
 
-
-
-print('shotest_route =', find_route(post_in, *addresses_in))
+if __name__ == '__main__':
+    post_in = (0, 2)
+    addresses_in = ((2, 5), (5, 2), (6, 6), (8, 3))
+    print('shortest_route =', find_route(post_in, addresses_in))
